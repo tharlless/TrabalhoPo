3a8776f44c68b88c4bd0d63e81fc69903bc48373
fernandoDCabral <fernando.dutra992@gmail.com>
2023-11-24 16:30:25 -0300

    Implementando o top10 grafico
    Implementando o top10 grafico

--- Cap05/src/br/com/mvbos/lgj/Ranking.java
+++ Cap05/src/br/com/mvbos/lgj/Ranking.java
@@ -1,5 +1,7 @@
 package br.com.mvbos.lgj;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -13,32 +15,12 @@ import java.util.Collections;
 public class Ranking {
     ArrayList<Jogadores> ranking = new ArrayList<>();
 
-
     public  void  addJogadores(Jogadores jogadores){
         ranking.add(jogadores);
         salvar_dado();
     }
-    public void organizar(){
-        Collections.sort(ranking);
-    }
-    public void tamanho_Lista_Top_10(){
-        while (ranking.size()>10){
-            ranking.remove(ranking.size()-1);
-        }
-    }
-
-    public void Limpar_Lista(){
-
-    }
-    public  void exibir_Top10(){
-        for (int i = 0; i<ranking.size();i++){
-            Jogadores jogador = ranking.get(i);
-            System.out.println((i + 1) + ". " + jogador.getNome() + " - Pontuação: " + jogador.getPontos());
-        }
-    }
     public void carregar_dados(){
-        //copia a pasta e coloca o caminho que você vai usar no final coloca // TOP10.txt. Neste ambiente não consigo criar.
-        Path top10 = Paths.get("C:\\Users\\fernando\\Desktop\\Ciencias da Comp\\2 semestre\\Programação orient objetos\\trabalho_Tharlles_e_Eu\\TrabalhoPo\\TOP10.txt");
+        Path top10 = Paths.get("C:\\Users\\fernando\\Desktop\\Cap05\\TOP10.txt");
         try(BufferedReader ler = new BufferedReader(new FileReader(top10.toFile()))) {
             if(!Files.exists(top10)){
                 Files.createFile(top10);
@@ -55,13 +37,38 @@ public class Ranking {
             System.out.println(e.getMessage());
         }
     }
+    public void organizar(){
+        Collections.sort(ranking);
+    }
+    public void tamanho_Lista_Top_10(){
+        while (ranking.size()>10){
+            ranking.remove(ranking.size()-1);
+        }
+    }
+    public  void exibir_Top10() {
+        JFrame exibir_Top10 = new JFrame("TOP 10");
+        exibir_Top10.setSize(500,672);
+
+        JTextArea Exibir_top10 = new JTextArea();
+        // tudo isso para usar um Ç e um ~
+        Font fonte_Usada = new Font("Arail",Font.PLAIN,12);
+        Exibir_top10.setFont(fonte_Usada);
+        //================================
+        for (int i = 0; i < ranking.size(); i++) {
+            Jogadores jogador = ranking.get(i);
+            Exibir_top10.append((i + 1) + ") Nome: " + jogador.getNome() + " ---- Pontos: " + jogador.getPontos()+"\n");
+            Exibir_top10.append("");
+        }
+        exibir_Top10.add(Exibir_top10);
+        exibir_Top10.setVisible(true);
+    }
+
     private void salvar_dado() {
-        //copia a pasta e coloca o caminho que você vai usar no final coloca // TOP10.txt. Neste ambiente não consigo criar.
-        Path top10 = Paths.get("C:\\Users\\fernando\\Desktop\\Ciencias da Comp\\2 semestre\\Programação orient objetos\\trabalho_Tharlles_e_Eu\\TrabalhoPo\\TOP10.txt");
+        Path top10 = Paths.get("C:\\Users\\fernando\\Desktop\\Cap05\\TOP10.txt");
         try(BufferedWriter escrever = new BufferedWriter(new FileWriter(top10.toFile()))) {
-           for(Jogadores jogadores : ranking){
-               escrever.write(jogadores.getNome()+","+jogadores.getPontos()+"\n");
-           }
+            for(Jogadores jogadores : ranking){
+                escrever.write(jogadores.getNome()+","+jogadores.getPontos()+"\n");
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
